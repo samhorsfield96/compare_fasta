2909035ff7f2c374f8c75a472485ce1328350a3e
Sam Horsfield <s.horsfield19@imperial.ac.uk>
2021-04-29 15:26:57 +0100

    Implements method for checking 3prime recall with old ggCaller output (colours within square brackets). Also removes old compare_fasta functions.

--- compare_fasta.py
+++ compare_fasta.py
@@ -1,262 +1,7 @@
 from Bio import SeqIO
 
-def compare_fasta(ref_fasta, query_fasta, type):
-    total_ref_records = 0
-    total_query_records = 0
-    total_correct_query_records = 0
-
-    ref_seq_list = []
-
-    #create list of all reference genes
-    for ref_rec in SeqIO.parse(ref_fasta, "fasta"):
-        total_ref_records += 1
-        ref_seq_list.append(str(ref_rec.seq))
-
-    unmatched_seq_list = ref_seq_list[:]
-
-    #iterate through query, checking if entry is in ref_seq_list, depending on whether running prodigal or ggCaller due to gene merging in ggCaller
-    if type == 'ggc':
-        for ggc_rec in SeqIO.parse(query_fasta, "fasta"):
-            total_query_records += 1
-            total_correct_query_records += ref_seq_list.count(str(ggc_rec.seq))
-            unmatched_seq_list = list(filter(lambda a: a != str(ggc_rec.seq), unmatched_seq_list))
-    elif type == 'prod':
-        for prod_rec in SeqIO.parse(query_fasta, "fasta"):
-            total_query_records += 1
-            if str(prod_rec.seq) in unmatched_seq_list:
-                total_correct_query_records += 1
-                unmatched_seq_list.remove(str(prod_rec.seq))
-
-
-
-    #calculate recall and precision
-    recall = total_correct_query_records / total_ref_records
-    precision = total_correct_query_records / total_query_records
-
-    print(query_fasta)
-    print("Recall: {}".format(recall))
-    print("Precision: {}".format(precision))
-    return(unmatched_seq_list)
-
-def compare_fasta_exact(ref_fasta, query_fasta, type, group):
-    total_ref_records = 0
-    total_query_records = 0
-    total_correct_query_records = 0
-
-    ref_seq_list = []
-
-    if group == "1":
-        group_list = ["CR931645.1", "CR931648.1", "CR931646.1", "CR931647.1"]
-    elif group == "2":
-        group_list = ["CR931719.1", "CR931658.1", "CR931659.1", "CR931660.1", "CR931717.1"]
-    elif group == "3":
-        group_list = ["CR931662.1", "CR931663.1", "CR931664.1", "CR931665.1", "CR931666.1"]
-
-    #create list of all reference genes
-    for ref_rec in SeqIO.parse(ref_fasta, "fasta"):
-        source_id = ((ref_rec.description.strip()).split("_"))[1]
-        total_ref_records += 1
-        ref_seq_list.append((source_id, str(ref_rec.seq)))
-
-    unmatched_seq_list = ref_seq_list[:]
-
-    #iterate through query, checking if entry is in ref_seq_list, depending on whether running prodigal or ggCaller due to gene merging in ggCaller
-    if type == 'ggc':
-        for ggc_rec in SeqIO.parse(query_fasta, "fasta"):
-            ORF_colours = (((((ggc_rec.description.strip()).split("["))[4]).replace("]", "")).replace("'", "")).replace(", ", "")
-            ORF_colours = list(ORF_colours)
-            for i in range(0, len(ORF_colours)):
-                if ORF_colours[i] == "1":
-                    ORF_pair = (group_list[i], str(ggc_rec.seq))
-                    total_query_records += 1
-                    if ORF_pair in unmatched_seq_list:
-                        total_correct_query_records += 1
-                        unmatched_seq_list.remove(ORF_pair)
-    elif type == 'prod':
-        for prod_rec in SeqIO.parse(query_fasta, "fasta"):
-            ORF_id = ((prod_rec.description.strip()).split("_"))[0] + ".1"
-            ORF_pair = (ORF_id, str(prod_rec.seq))
-            total_query_records += 1
-            if ORF_pair in unmatched_seq_list:
-                total_correct_query_records += 1
-                unmatched_seq_list.remove(ORF_pair)
-
-
-
-    #calculate recall and precision
-    recall = total_correct_query_records / total_ref_records
-    precision = total_correct_query_records / total_query_records
-
-    print(query_fasta)
-    print("Total ORFs: {}".format(total_query_records))
-    print("Recall: {}".format(recall))
-    print("Precision: {}".format(precision))
-    return(unmatched_seq_list)
-
-def compare_fasta_py_cpp(ref_fasta, query_fasta, type, group):
-    total_ref_records = 0
-    total_query_records = 0
-    total_correct_query_records = 0
-
-    ref_seq_list = []
-
-    if group == "1":
-        group_list = ["CR931645.1", "CR931648.1", "CR931646.1", "CR931647.1"]
-    elif group == "2":
-        group_list = ["CR931719.1", "CR931658.1", "CR931659.1", "CR931660.1", "CR931717.1"]
-    elif group == "3":
-        group_list = ["CR931662.1", "CR931663.1", "CR931664.1", "CR931665.1", "CR931666.1"]
-
-    #create list of all reference genes
-    for py_rec in SeqIO.parse(ref_fasta, "fasta"):
-        ORF_colours = (((((py_rec.description.strip()).split("["))[4]).replace("]", "")).replace("'", "")).replace(
-            ", ", "")
-        ORF_colours = list(ORF_colours)
-        for i in range(0, len(ORF_colours)):
-            if ORF_colours[i] == "1":
-                ORF_pair = (group_list[i], str(py_rec.seq))
-                total_ref_records += 1
-                ref_seq_list.append(ORF_pair)
-
-    unmatched_seq_list = ref_seq_list[:]
-
-    #iterate through query, checking if entry is in ref_seq_list, depending on whether running prodigal or ggCaller due to gene merging in ggCaller
-    if type == 'ggc':
-        for ggc_rec in SeqIO.parse(query_fasta, "fasta"):
-            ORF_description = (ggc_rec.description).split("_")
-            ORF_colours = list(ORF_description[2])
-            for i in range(0, len(ORF_colours)):
-                if ORF_colours[i] == "1":
-                    ORF_pair = (group_list[i], str(ggc_rec.seq))
-                    total_query_records += 1
-                    if ORF_pair in unmatched_seq_list:
-                        total_correct_query_records += 1
-                        unmatched_seq_list.remove(ORF_pair)
-    elif type == 'prod':
-        for prod_rec in SeqIO.parse(query_fasta, "fasta"):
-            ORF_id = ((prod_rec.description.strip()).split("_"))[0] + ".1"
-            ORF_pair = (ORF_id, str(prod_rec.seq))
-            total_query_records += 1
-            if ORF_pair in unmatched_seq_list:
-                total_correct_query_records += 1
-                unmatched_seq_list.remove(ORF_pair)
-
-
-
-    #calculate recall and precision
-    recall = total_correct_query_records / total_ref_records
-    precision = total_correct_query_records / total_query_records
-
-    print(query_fasta)
-    print("Total ORFs: {}".format(total_query_records))
-    print("Recall: {}".format(recall))
-    print("Precision: {}".format(precision))
-    return(unmatched_seq_list)
-
-def compare_fasta_cpp_cpp(ref_fasta, query_fasta, group):
-    total_ref_records = 0
-    total_query_records = 0
-    total_correct_query_records = 0
-
-    ref_seq_list = []
-
-    if group == "1":
-        group_list = ["CR931645.1", "CR931648.1", "CR931646.1", "CR931647.1"]
-    elif group == "2":
-        group_list = ["CR931719.1", "CR931658.1", "CR931659.1", "CR931660.1", "CR931717.1"]
-    elif group == "3":
-        group_list = ["CR931662.1", "CR931663.1", "CR931664.1", "CR931665.1", "CR931666.1"]
-
-    #create list of all reference genes
-    for ref_rec in SeqIO.parse(ref_fasta, "fasta"):
-        ORF_description = (ref_rec.description).split("_")
-        ORF_colours = list(ORF_description[2])
-        for i in range(0, len(ORF_colours)):
-            if ORF_colours[i] == "1":
-                ORF_pair = (group_list[i], str(ref_rec.seq))
-                total_ref_records += 1
-                ref_seq_list.append(ORF_pair)
-
-    unmatched_seq_list = ref_seq_list[:]
-
-    #iterate through query, checking if entry is in ref_seq_list, depending on whether running prodigal or ggCaller due to gene merging in ggCaller
-    for query_rec in SeqIO.parse(query_fasta, "fasta"):
-        ORF_description = (query_rec.description).split("_")
-        ORF_colours = list(ORF_description[2])
-        for i in range(0, len(ORF_colours)):
-            if ORF_colours[i] == "1":
-                ORF_pair = (group_list[i], str(query_rec.seq))
-                total_query_records += 1
-                if ORF_pair in unmatched_seq_list:
-                    total_correct_query_records += 1
-                    unmatched_seq_list.remove(ORF_pair)
-
-    #calculate recall and precision
-    recall = total_correct_query_records / total_ref_records
-    precision = total_correct_query_records / total_query_records
-
-    print(query_fasta)
-    print("Total ORFs: {}".format(total_query_records))
-    print("Recall: {}".format(recall))
-    print("Precision: {}".format(precision))
-    return(unmatched_seq_list)
-
-def compare_fasta_exact_cpp(ref_fasta, query_fasta, type, group):
-    total_ref_records = 0
-    total_query_records = 0
-    total_correct_query_records = 0
-
-    ref_seq_list = []
-
-    if group == "1":
-        group_list = ["CR931645.1", "CR931648.1", "CR931646.1", "CR931647.1"]
-    elif group == "2":
-        group_list = ["CR931719.1", "CR931658.1", "CR931659.1", "CR931660.1", "CR931717.1"]
-    elif group == "3":
-        group_list = ["CR931662.1", "CR931663.1", "CR931664.1", "CR931665.1", "CR931666.1"]
-
-    #create list of all reference genes
-    for ref_rec in SeqIO.parse(ref_fasta, "fasta"):
-        source_id = ((ref_rec.description.strip()).split("_"))[1]
-        total_ref_records += 1
-        ref_seq_list.append((source_id, str(ref_rec.seq)))
-
-    unmatched_seq_list = ref_seq_list[:]
-
-    #iterate through query, checking if entry is in ref_seq_list, depending on whether running prodigal or ggCaller due to gene merging in ggCaller
-    if type == 'ggc':
-        for ggc_rec in SeqIO.parse(query_fasta, "fasta"):
-            ORF_description = (ggc_rec.description).split("_")
-            ORF_colours = list(ORF_description[2])
-            for i in range(0, len(ORF_colours)):
-                if ORF_colours[i] == "1":
-                    ORF_pair = (group_list[i], str(ggc_rec.seq))
-                    total_query_records += 1
-                    if ORF_pair in unmatched_seq_list:
-                        total_correct_query_records += 1
-                        unmatched_seq_list.remove(ORF_pair)
-    elif type == 'prod':
-        for prod_rec in SeqIO.parse(query_fasta, "fasta"):
-            ORF_id = ((prod_rec.description.strip()).split("_"))[0] + ".1"
-            ORF_pair = (ORF_id, str(prod_rec.seq))
-            total_query_records += 1
-            if ORF_pair in unmatched_seq_list:
-                total_correct_query_records += 1
-                unmatched_seq_list.remove(ORF_pair)
-
-
-
-    #calculate recall and precision
-    recall = total_correct_query_records / total_ref_records
-    precision = total_correct_query_records / total_query_records
-
-    print(query_fasta)
-    print("Total ORFs: {}".format(total_query_records))
-    print("Recall: {}".format(recall))
-    print("Precision: {}".format(precision))
-    return(unmatched_seq_list)
-
-def compare_3prime(genome_fasta, ref_fasta, query_fasta, caller_type, min_size):
+# used for analysing old ggCaller output (colours held in square brackets)
+def compare_3prime_old(genome_fasta, ref_fasta, query_fasta, caller_type, min_size):
     genome_list = []
     genome_rec = {}
     ref_rec = {}
@@ -268,7 +13,7 @@ def compare_3prime(genome_fasta, ref_fasta, query_fasta, caller_type, min_size):
 
     unmatched_query_list = []
     unmatched_ref_list = []
-
+    incorrect_query_list = []
 
     # parse genome_fasta
     for rec in SeqIO.parse(genome_fasta, "fasta"):
@@ -282,7 +27,112 @@ def compare_3prime(genome_fasta, ref_fasta, query_fasta, caller_type, min_size):
     # parse ref_fasta
     for rec in SeqIO.parse(ref_fasta, "fasta"):
         description = (rec.description).split("_")
+        id = description[1]
+
+        if len(str(rec.seq)) < min_size:
+           continue
+
+
+        # look for the 3prime index of the string
+        start_index = genome_rec[id].find(str(rec.seq))
+        if start_index == -1:
+            prime3 = genome_rec[id].find(str(rec.seq.reverse_complement()))
+        else:
+            prime3 = start_index + (len(str(rec.seq)) - 1)
+        #check that sequence is present in genome is says, and that the gene sequence is valid and no Ns present. Also check that no duplicate entries, as ggc can't call duplicate genes
+        if prime3 != -1 and remove_invalid(str(rec.seq)) and "N" not in str(rec.seq) and prime3 not in ref_rec[id]:
+            ref_rec[id][prime3] = str(rec.seq)
+            total_ref_records += 1
+            unmatched_ref_list.append((id, str(rec.seq)))
+
+    if caller_type == "ggc":
+        # parse query_fasta
+        for rec in SeqIO.parse(query_fasta, "fasta"):
+            colours = (((((rec.description.strip()).split("["))[4]).replace("]", "")).replace("'", "")).replace(", ", "")
+            colours = list(colours)
+
+            for index, col in enumerate(colours):
+                if col == "1":
+                    id = genome_list[index]
+                    # look for the 3prime index of the string
+                    start_index = genome_rec[id].find(str(rec.seq))
+                    if start_index == -1:
+                        prime3 = genome_rec[id].find(str(rec.seq.reverse_complement()))
+                    else:
+                        prime3 = start_index + (len(str(rec.seq)) - 1)
+
+                    # determine if gene sequence is real, if not add to incorrect_query_list
+                    if prime3 != -1:
+                        query_rec[id][prime3] = str(rec.seq)
+                    else:
+                        incorrect_query_list.append((id, str(rec.seq)))
+                    total_query_records += 1
+
+    elif caller_type == "prod":
+        # parse query_fasta
+        for rec in SeqIO.parse(query_fasta, "fasta"):
+            description = (rec.description).split("_")
+            id = description[0]
+
+            # look for the 3prime index of the string
+            start_index = genome_rec[id].find(str(rec.seq))
+            if start_index == -1:
+                prime3 = genome_rec[id].find(str(rec.seq.reverse_complement()))
+            else:
+                prime3 = start_index + (len(str(rec.seq)) - 1)
+            if remove_invalid(str(rec.seq)) and "N" not in str(rec.seq) and prime3 not in query_rec[id]:
+                query_rec[id][prime3] = str(rec.seq)
+            else:
+                incorrect_query_list.append((id, str(rec.seq)))
+            total_query_records += 1
+
+    # iterate over query_rec, count number of times each 3prime match found
+    for colour, prime3_dict in query_rec.items():
+        for prime3_key in prime3_dict.keys():
+            if prime3_key in ref_rec[colour]:
+                total_correct_query_records += 1
+                unmatched_ref_list.remove((colour, ref_rec[colour][prime3_key]))
+            else:
+                unmatched = (colour, prime3_dict[prime3_key])
+                unmatched_query_list.append(unmatched)
+
+
+    recall = total_correct_query_records / total_ref_records
+    precision = total_correct_query_records / total_query_records
+
+    print("Total ORFs: {}".format(total_query_records))
+    print("Recall: {}".format(recall))
+    print("Precision: {}".format(precision))
+    return(unmatched_ref_list, unmatched_query_list, incorrect_query_list)
+
+# used for analysing new ggCaller output (colours binary array)
+def compare_3prime_new(genome_fasta, ref_fasta, query_fasta, caller_type, min_size):
+    genome_list = []
+    genome_rec = {}
+    ref_rec = {}
+    query_rec = {}
+
+    total_correct_query_records = 0
+    total_ref_records = 0
+    total_query_records = 0
+
+    unmatched_query_list = []
+    unmatched_ref_list = []
+    incorrect_query_list = []
+
+    # parse genome_fasta
+    for rec in SeqIO.parse(genome_fasta, "fasta"):
+        description = (rec.description).split("_")
         id = description[0]
+        genome_list.append(id)
+        genome_rec[id] = str(rec.seq)
+        ref_rec[id] = {}
+        query_rec[id] = {}
+
+    # parse ref_fasta
+    for rec in SeqIO.parse(ref_fasta, "fasta"):
+        description = (rec.description).split("_")
+        id = description[1]
 
         if len(str(rec.seq)) < min_size:
            continue
@@ -315,7 +165,12 @@ def compare_3prime(genome_fasta, ref_fasta, query_fasta, caller_type, min_size):
                         prime3 = genome_rec[id].find(str(rec.seq.reverse_complement()))
                     else:
                         prime3 = start_index + (len(str(rec.seq)) - 1)
-                    query_rec[id][prime3] = str(rec.seq)
+
+                    # determine if gene sequence is real, if not add to incorrect_query_list
+                    if prime3 != -1:
+                        query_rec[id][prime3] = str(rec.seq)
+                    else:
+                        incorrect_query_list.append((id, str(rec.seq)))
                     total_query_records += 1
 
     elif caller_type == "prod":
@@ -332,7 +187,9 @@ def compare_3prime(genome_fasta, ref_fasta, query_fasta, caller_type, min_size):
                 prime3 = start_index + (len(str(rec.seq)) - 1)
             if remove_invalid(str(rec.seq)) and "N" not in str(rec.seq) and prime3 not in query_rec[id]:
                 query_rec[id][prime3] = str(rec.seq)
-                total_query_records += 1
+            else:
+                incorrect_query_list.append((id, str(rec.seq)))
+            total_query_records += 1
 
     # iterate over query_rec, count number of times each 3prime match found
     for colour, prime3_dict in query_rec.items():
@@ -351,7 +208,7 @@ def compare_3prime(genome_fasta, ref_fasta, query_fasta, caller_type, min_size):
     print("Total ORFs: {}".format(total_query_records))
     print("Recall: {}".format(recall))
     print("Precision: {}".format(precision))
-    return(unmatched_ref_list, unmatched_query_list)
+    return(unmatched_ref_list, unmatched_query_list, incorrect_query_list)
 
 def remove_invalid(query_seq):
     import re
@@ -383,6 +240,6 @@ def select_seq_length(infasta, outfasta, length):
 if __name__ == '__main__':
     from Bio import SeqIO
     #unmatched_ref, unmatched_query = compare_3prime("clique_556_seqs_all.fasta", "clique_556_CDS_all.fasta", "clique556_calls_4threads_post_unitigDict_alteration_3.fasta", "ggc", 90)
-    unmatched_ref_post, unmatched_query_post = compare_3prime("clique_556_seqs_all.fasta", "clique_556_CDS_all.fasta",
+    unmatched_ref_post, unmatched_query_post = compare_3prime_old("clique_556_seqs_all.fasta", "clique_556_CDS_all.fasta",
                                                               "clique_556_list_chunking_4threads_filtered.fasta", "ggc",
                                                               90)
