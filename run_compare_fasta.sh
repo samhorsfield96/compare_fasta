c2feac0916da03f2d75eab9fda84d7c37733d5c4
Sam Horsfield <s.horsfield19@imperial.ac.uk>
2023-04-08 13:19:14 +0100

    Adds comparisons between ggcaller and panaroo false positives.

--- run_compare_fasta.sh
+++ run_compare_fasta.sh
@@ -56,21 +56,40 @@
 #python compare_fasta.py --seq Plasmid_data/clique_119_230_372_seq_all.fasta --genes Plasmid_data/clique_119_230_372_CDS_all.fasta --query prodigal_outputs/plasmid_clique_119_230_372_prodigal.fa --caller prod --vers 1.3 --outpref results/Diverse_plasmids_prod
 #python compare_fasta.py --seq Plasmid_data/clique_119_230_372_seq_all.fasta --genes Plasmid_data/clique_119_230_372_CDS_all.fasta --query prodigal_outputs/plasmid_clique_119_230_372_prodigal.fa --caller prod --vers 1.3 --outpref results/Diverse_plasmids_prod_exact --exact
 
-# compare false positives against eachother
-python compare_fasta.py --seq Pneumo_capsular_data/group1_capsular_seqs.fasta --genes results/panaroo/group1_CBL_pan_exact_FP.fasta --query results/ggCaller/group1_CBL_ggc_1_3_4_exact_FP.fasta --caller ggc --vers 1.3 --outpref results/group1_CBL_ggc_1_3_4_v_pan
-python compare_fasta.py --seq Pneumo_capsular_data/group1_capsular_seqs.fasta --genes results/panaroo/group1_CBL_pan_exact_FP.fasta --query results/ggCaller/group1_CBL_ggc_1_3_4_exact_FP.fasta --caller ggc --vers 1.3 --outpref results/group1_CBL_ggc_1_3_4_v_pan_exact --exact
+# compare false positives against eachother (ggcaller (query) vs panaroo (reference))
+#python compare_fasta.py --seq Pneumo_capsular_data/group1_capsular_seqs.fasta --genes results/panaroo/group1_CBL_pan_exact_FP.fasta --query results/ggCaller/group1_CBL_ggc_1_3_4_exact_FP.fasta --caller ggc --vers 1.3 --outpref results/group1_CBL_ggc_1_3_4_v_pan
+#python compare_fasta.py --seq Pneumo_capsular_data/group1_capsular_seqs.fasta --genes results/panaroo/group1_CBL_pan_exact_FP.fasta --query results/ggCaller/group1_CBL_ggc_1_3_4_exact_FP.fasta --caller ggc --vers 1.3 --outpref results/group1_CBL_ggc_1_3_4_v_pan_exact --exact
 
-python compare_fasta.py --seq Pneumo_capsular_data/group2_capsular_seqs.fasta --genes results/panaroo/group2_CBL_pan_exact_FP.fasta --query results/ggCaller/group2_CBL_ggc_1_3_4_exact_FP.fasta --caller ggc --vers 1.3 --outpref results/group2_CBL_ggc_1_3_4_v_pan
-python compare_fasta.py --seq Pneumo_capsular_data/group2_capsular_seqs.fasta --genes results/panaroo/group2_CBL_pan_exact_FP.fasta --query results/ggCaller/group2_CBL_ggc_1_3_4_exact_FP.fasta --caller ggc --vers 1.3 --outpref results/group2_CBL_ggc_1_3_4_v_pan_exact --exact
+#python compare_fasta.py --seq Pneumo_capsular_data/group2_capsular_seqs.fasta --genes results/panaroo/group2_CBL_pan_exact_FP.fasta --query results/ggCaller/group2_CBL_ggc_1_3_4_exact_FP.fasta --caller ggc --vers 1.3 --outpref results/group2_CBL_ggc_1_3_4_v_pan
+#python compare_fasta.py --seq Pneumo_capsular_data/group2_capsular_seqs.fasta --genes results/panaroo/group2_CBL_pan_exact_FP.fasta --query results/ggCaller/group2_CBL_ggc_1_3_4_exact_FP.fasta --caller ggc --vers 1.3 --outpref results/group2_CBL_ggc_1_3_4_v_pan_exact --exact
 
-python compare_fasta.py --seq Pneumo_capsular_data/group3_capsular_seqs.fasta --genes results/panaroo/group3_CBL_pan_exact_FP.fasta --query results/ggCaller/group3_CBL_ggc_1_3_4_exact_FP.fasta --caller ggc --vers 1.3 --outpref results/group3_CBL_ggc_1_3_4_v_pan
-python compare_fasta.py --seq Pneumo_capsular_data/group3_capsular_seqs.fasta --genes results/panaroo/group3_CBL_pan_exact_FP.fasta --query results/ggCaller/group3_CBL_ggc_1_3_4_exact_FP.fasta --caller ggc --vers 1.3 --outpref results/group3_CBL_ggc_1_3_4_v_pan_exact --exact
+#python compare_fasta.py --seq Pneumo_capsular_data/group3_capsular_seqs.fasta --genes results/panaroo/group3_CBL_pan_exact_FP.fasta --query results/ggCaller/group3_CBL_ggc_1_3_4_exact_FP.fasta --caller ggc --vers 1.3 --outpref results/group3_CBL_ggc_1_3_4_v_pan
+#python compare_fasta.py --seq Pneumo_capsular_data/group3_capsular_seqs.fasta --genes results/panaroo/group3_CBL_pan_exact_FP.fasta --query results/ggCaller/group3_CBL_ggc_1_3_4_exact_FP.fasta --caller ggc --vers 1.3 --outpref results/group3_CBL_ggc_1_3_4_v_pan_exact --exact
 
-python compare_fasta.py --seq Pneumo_capsular_data/all_capsular_seqs.fasta --genes results/panaroo/all_CBL_pan_exact_FP.fasta --query results/ggCaller/all_CBL_ggc_1_3_4_exact_FP.fasta --caller ggc --vers 1.3 --outpref results/all_CBL_ggc_1_3_4_v_pan
-python compare_fasta.py --seq Pneumo_capsular_data/all_capsular_seqs.fasta --genes results/panaroo/all_CBL_pan_exact_FP.fasta --query results/ggCaller/all_CBL_ggc_1_3_4_exact_FP.fasta --caller ggc --vers 1.3 --outpref results/all_CBL_ggc_1_3_4_v_pan_exact --exact
+#python compare_fasta.py --seq Pneumo_capsular_data/all_capsular_seqs.fasta --genes results/panaroo/all_CBL_pan_exact_FP.fasta --query results/ggCaller/all_CBL_ggc_1_3_4_exact_FP.fasta --caller ggc --vers 1.3 --outpref results/all_CBL_ggc_1_3_4_v_pan
+#python compare_fasta.py --seq Pneumo_capsular_data/all_capsular_seqs.fasta --genes results/panaroo/all_CBL_pan_exact_FP.fasta --query results/ggCaller/all_CBL_ggc_1_3_4_exact_FP.fasta --caller ggc --vers 1.3 --outpref results/all_CBL_ggc_1_3_4_v_pan_exact --exact
 
-python compare_fasta.py --seq Plasmid_data/clique_556_seqs_all.fasta --genes results/panaroo/Undiverse_plasmids_pan_exact_FP.fasta --query results/ggCaller/Undiverse_plasmids_ggc_1_3_4_exact_FP.fasta --caller ggc --vers 1.3 --outpref results/Undiverse_plasmids_ggc_1_3_4_v_pan
-python compare_fasta.py --seq Plasmid_data/clique_556_seqs_all.fasta --genes results/panaroo/Undiverse_plasmids_pan_exact_FP.fasta --query results/ggCaller/Undiverse_plasmids_ggc_1_3_4_exact_FP.fasta --caller ggc --vers 1.3 --outpref results/Undiverse_plasmids_ggc_1_3_4_v_pan_exact --exact
+#python compare_fasta.py --seq Plasmid_data/clique_556_seqs_all.fasta --genes results/panaroo/Undiverse_plasmids_pan_exact_FP.fasta --query results/ggCaller/Undiverse_plasmids_ggc_1_3_4_exact_FP.fasta --caller ggc --vers 1.3 --outpref results/Undiverse_plasmids_ggc_1_3_4_v_pan
+#python compare_fasta.py --seq Plasmid_data/clique_556_seqs_all.fasta --genes results/panaroo/Undiverse_plasmids_pan_exact_FP.fasta --query results/ggCaller/Undiverse_plasmids_ggc_1_3_4_exact_FP.fasta --caller ggc --vers 1.3 --outpref results/Undiverse_plasmids_ggc_1_3_4_v_pan_exact --exact
 
-python compare_fasta.py --seq Plasmid_data/clique_119_230_372_seq_all.fasta --genes results/panaroo/Diverse_plasmids_pan_exact_FP.fasta --query results/ggCaller/Diverse_plasmids_ggc_1_3_4_exact_FP.fasta --caller ggc --vers 1.3 --outpref results/Diverse_plasmids_ggc_1_3_4_v_pan
-python compare_fasta.py --seq Plasmid_data/clique_119_230_372_seq_all.fasta --genes results/panaroo/Diverse_plasmids_pan_exact_FP.fasta --query results/ggCaller/Diverse_plasmids_ggc_1_3_4_exact_FP.fasta --caller ggc --vers 1.3 --outpref results/Diverse_plasmids_ggc_1_3_4_v_pan_exact --exact
+#python compare_fasta.py --seq Plasmid_data/clique_119_230_372_seq_all.fasta --genes results/panaroo/Diverse_plasmids_pan_exact_FP.fasta --query results/ggCaller/Diverse_plasmids_ggc_1_3_4_exact_FP.fasta --caller ggc --vers 1.3 --outpref results/Diverse_plasmids_ggc_1_3_4_v_pan
+#python compare_fasta.py --seq Plasmid_data/clique_119_230_372_seq_all.fasta --genes results/panaroo/Diverse_plasmids_pan_exact_FP.fasta --query results/ggCaller/Diverse_plasmids_ggc_1_3_4_exact_FP.fasta --caller ggc --vers 1.3 --outpref results/Diverse_plasmids_ggc_1_3_4_v_pan_exact --exact
+
+# compare false positives against eachother (panaroo (query) vs ggcaller (reference))
+python compare_fasta.py --seq Pneumo_capsular_data/group1_capsular_seqs.fasta --genes results/ggCaller/group1_CBL_ggc_1_3_4_exact_FP.fasta --query results/panaroo/group1_CBL_pan_exact_FP.fasta --caller pan --vers 1.3 --outpref results/group1_CBL_pan_v_ggc_1_3_4
+python compare_fasta.py --seq Pneumo_capsular_data/group1_capsular_seqs.fasta --genes results/ggCaller/group1_CBL_ggc_1_3_4_exact_FP.fasta --query results/panaroo/group1_CBL_pan_exact_FP.fasta --caller pan --vers 1.3 --outpref results/group1_CBL_pan_v_ggc_1_3_4_exact --exact
+
+python compare_fasta.py --seq Pneumo_capsular_data/group2_capsular_seqs.fasta --genes results/ggCaller/group2_CBL_ggc_1_3_4_exact_FP.fasta --query results/panaroo/group2_CBL_pan_exact_FP.fasta --caller pan --vers 1.3 --outpref results/group2_CBL_pan_v_ggc_1_3_4
+python compare_fasta.py --seq Pneumo_capsular_data/group2_capsular_seqs.fasta --genes results/ggCaller/group2_CBL_ggc_1_3_4_exact_FP.fasta --query results/panaroo/group2_CBL_pan_exact_FP.fasta --caller pan --vers 1.3 --outpref results/group2_CBL_pan_v_ggc_1_3_4_exact --exact
+
+python compare_fasta.py --seq Pneumo_capsular_data/group3_capsular_seqs.fasta --genes results/ggCaller/group3_CBL_ggc_1_3_4_exact_FP.fasta --query results/panaroo/group3_CBL_pan_exact_FP.fasta --caller pan --vers 1.3 --outpref results/group3_CBL_pan_v_ggc_1_3_4
+python compare_fasta.py --seq Pneumo_capsular_data/group3_capsular_seqs.fasta --genes results/ggCaller/group3_CBL_ggc_1_3_4_exact_FP.fasta --query results/panaroo/group3_CBL_pan_exact_FP.fasta --caller pan --vers 1.3 --outpref results/group3_CBL_pan_v_ggc_1_3_4_exact --exact
+
+python compare_fasta.py --seq Pneumo_capsular_data/all_capsular_seqs.fasta --genes results/ggCaller/all_CBL_ggc_1_3_4_exact_FP.fasta --query results/panaroo/all_CBL_pan_exact_FP.fasta --caller pan --vers 1.3 --outpref results/all_CBL_pan_v_ggc_1_3_4
+python compare_fasta.py --seq Pneumo_capsular_data/all_capsular_seqs.fasta --genes results/ggCaller/all_CBL_ggc_1_3_4_exact_FP.fasta --query results/panaroo/all_CBL_pan_exact_FP.fasta --caller pan --vers 1.3 --outpref results/all_CBL_pan_v_ggc_1_3_4_exact --exact
+
+python compare_fasta.py --seq Plasmid_data/clique_556_seqs_all.fasta --genes results/ggCaller/Undiverse_plasmids_ggc_1_3_4_exact_FP.fasta --query results/panaroo/Undiverse_plasmids_pan_exact_FP.fasta --caller pan --vers 1.3 --outpref results/Undiverse_plasmids_pan_v_ggc_1_3_4
+python compare_fasta.py --seq Plasmid_data/clique_556_seqs_all.fasta --genes results/ggCaller/Undiverse_plasmids_ggc_1_3_4_exact_FP.fasta --query results/panaroo/Undiverse_plasmids_pan_exact_FP.fasta --caller pan --vers 1.3 --outpref results/Undiverse_plasmids_pan_v_ggc_1_3_4_exact --exact
+
+python compare_fasta.py --seq Plasmid_data/clique_119_230_372_seq_all.fasta --genes results/ggCaller/Diverse_plasmids_ggc_1_3_4_exact_FP.fasta --query results/panaroo/Diverse_plasmids_pan_exact_FP.fasta --caller pan --vers 1.3 --outpref results/Diverse_plasmids_pan_v_ggc_1_3_4
+python compare_fasta.py --seq Plasmid_data/clique_119_230_372_seq_all.fasta --genes results/ggCaller/Diverse_plasmids_ggc_1_3_4_exact_FP.fasta --query results/panaroo/Diverse_plasmids_pan_exact_FP.fasta --caller pan --vers 1.3 --outpref results/Diverse_plasmids_pan_v_ggc_1_3_4_exact --exact
